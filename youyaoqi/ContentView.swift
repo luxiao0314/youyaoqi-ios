5723965471849fac8659ed292abc5cd63f7ceca0
xiao.lu <luxiao@qutoutiao.net>
2020-07-19 10:28:16 +0800

    Initial Commit

---
+++ youyaoqi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  youyaoqi
+//
+//  Created by lucio on 2020/7/19.
+//  Copyright © 2020 lucio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

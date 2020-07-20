eae0b3cbceb9e1833f6469eba78b9220342fbc1b
lux <xiao.lu@aihuishou.com>
2020-07-20 14:22:51 +0800

    增加分类接口

--- youyaoqi/Procedure/Global.swift
+++ youyaoqi/Procedure/Global.swift
@@ -1,9 +1,9 @@
 //
 //  Global.swift
-//  youyaoqi
+//  U17
 //
-//  Created by lucio on 2020/7/19.
-//  Copyright © 2020 lucio. All rights reserved.
+//  Created by charles on 2017/10/24.
+//  Copyright © 2017年 None. All rights reserved.
 //
 
 import Foundation
@@ -22,3 +22,95 @@ extension UIColor {
         return UIColor(r: 29, g: 221, b: 43)
     }
 }
+
+extension String {
+    static let searchHistoryKey = "searchHistoryKey"
+    static let sexTypeKey = "sexTypeKey"
+}
+
+extension NSNotification.Name {
+    static let USexTypeDidChange = NSNotification.Name("USexTypeDidChange")
+}
+
+let screenWidth = UIScreen.main.bounds.width
+let screenHeight = UIScreen.main.bounds.height
+
+var topVC: UIViewController? {
+    var resultVC: UIViewController?
+    resultVC = _topVC(UIApplication.shared.keyWindow?.rootViewController)
+    while resultVC?.presentedViewController != nil {
+        resultVC = _topVC(resultVC?.presentedViewController)
+    }
+    return resultVC
+}
+
+var isIphoneX: Bool {
+    return UI_USER_INTERFACE_IDIOM() == .phone
+        && (max(UIScreen.main.bounds.height, UIScreen.main.bounds.width) == 812
+        || max(UIScreen.main.bounds.height, UIScreen.main.bounds.width) == 896)
+}
+
+private  func _topVC(_ vc: UIViewController?) -> UIViewController? {
+    if vc is UINavigationController {
+        return _topVC((vc as? UINavigationController)?.topViewController)
+    } else if vc is UITabBarController {
+        return _topVC((vc as? UITabBarController)?.selectedViewController)
+    } else {
+        return vc
+    }
+}
+
+//MARK: print
+func uLog<T>(_ message: T, file: String = #file, function: String = #function, lineNumber: Int = #line) {
+    #if DEBUG
+        let fileName = (file as NSString).lastPathComponent
+        print("[\(fileName):funciton:\(function):line:\(lineNumber)]- \(message)")
+    #endif
+}
+
+//MARK: Kingfisher
+extension Kingfisher where Base: ImageView {
+    @discardableResult
+    public func setImage(urlString: String?, placeholder: Placeholder? = UIImage(named: "normal_placeholder_h")) -> RetrieveImageTask {
+        return setImage(with: URL(string: urlString ?? ""),
+                        placeholder: placeholder,
+                        options:[.transition(.fade(0.5))])
+    }
+}
+
+extension Kingfisher where Base: UIButton {
+    @discardableResult
+    public func setImage(urlString: String?, for state: UIControl.State, placeholder: UIImage? = UIImage(named: "normal_placeholder_h")) -> RetrieveImageTask {
+        return setImage(with: URL(string: urlString ?? ""),
+                        for: state,
+                        placeholder: placeholder,
+                        options: [.transition(.fade(0.5))])
+        
+    }
+}
+//MARK: SnapKit
+extension ConstraintView {
+    
+    var usnp: ConstraintBasicAttributesDSL {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.snp
+        } else {
+            return self.snp
+        }
+    }
+}
+
+extension UICollectionView {
+    
+    func reloadData(animation: Bool = true) {
+        if animation {
+            reloadData()
+        } else {
+            UIView .performWithoutAnimation {
+                reloadData()
+            }
+        }
+    }
+}
+
+
